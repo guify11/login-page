8aea441e70594682667039dd524d31713c147ec3
Guilherme Silva Ferreira <guify.kmail@gmail.com>
2026-07-14 15:15:13 +0100

    Added a system to change language

--- script.js
+++ script.js
@@ -10,5 +10,27 @@ function register() {
     alert(String("welcome, ") + String(localStorage.getItem("name")))
 }
 function atualizartexto() {
-    document.getElementById("WelcomeMessage").innerText = String("welcome: " + localStorage.getItem("name"))
+    if (localStorage.getItem("lang") == null){
+        localStorage.setItem("lang", "en")
+    }
+    if (localStorage.getItem("lang") == "en"){
+        if (window.name == "start"){
+           document.getElementById("WelcomeMessage").innerText = String("welcome: " + localStorage.getItem("name")) 
+        }
+        else if (window.name == "index") {
+            document.getElementById("greatings").innerText = "welcome"
+            document.getElementById("log-in").innerText = "log-in"
+        }
+        else if (window.name == "register") {
+            document.getElementById("greatings").innerText = "welcome"
+            document.getElementById("register").innerText = "register: "
+            document.getElementById("registerButt").innerText = "register"
+        }
+    }
+    
+}
+
+function chancheLang(Lang) {
+    localStorage.setItem("lang", Lang)
+    console.log(String("language change to ") + String(Lang))
 }
